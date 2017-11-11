af9972086b50d5f19d262954b240c29e7ab92e7b
silentarrowz <silentarrowz@gmail.com>
2017-11-11 13:14:53 +0530

    router middleware added

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var things = require('./things.js');
+
+
+//Simple request time logger
+app.use( '/things' ,function(req, res, next){
+    console.log("A new request received at " + Date.now());
+    
+    //This function call is very important. It tells that more processing is
+    //required for the current request and is in the next middleware function/route handler.
+    
+    next();
+ });
+ 
+
+app.use('/things',things);
 
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname + '/index.html'));
@@ -8,6 +23,14 @@ app.get('/',function(req,res){
 
 app.get('/hello',function(req,res){
     res.send('Hello World');
+});
+
+app.get('/:id',function(req,res){
+    res.send('The id you specified is : '+req.params.id);
+})
+
+app.post('/hello',function(req,res){
+    res.send('You just called the post at hello route');
 });
 
 app.listen(3000);
